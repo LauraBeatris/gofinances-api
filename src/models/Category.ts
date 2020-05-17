2d69ea3fcc98e44cf3272f19fe80b06b4b25859c
Laura <laurabeatriserafim@gmail.com>
2020-05-17 14:59:18 -0300

    feat: map entities to database

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  @JoinColumn({
+    name: 'category_id',
+    referencedColumnName: 'category_id',
+  })
+  transactions: Transaction[];
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
   created_at: Date;
 
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
   updated_at: Date;
 }
 
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,54 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+export type TransactionType = 'income' | 'outcome';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column({
+    type: 'enum',
+    enum: ['income', 'outcome'],
+  })
+  type: TransactionType;
 
+  @Column('int')
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @ManyToOne(() => Category, {
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({
+    name: 'category_id',
+    referencedColumnName: 'id',
+  })
+  category: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
   created_at: Date;
 
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
   updated_at: Date;
 }
 
