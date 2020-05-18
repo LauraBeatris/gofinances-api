30a8c18eec574bc3bd3b8e169f99d5735dab592b
Laura <laurabeatriserafim@gmail.com>
2020-05-17 23:19:31 -0300

    feat: add delete transaction service

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isUuid } from 'uuidv4';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    if (!id || !isUuid(id)) {
+      throw new AppError('Provide a valid transaction id');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    await transactionsRepository.delete(id);
   }
 }
 
