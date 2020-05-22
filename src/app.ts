cef9dda79f0f0ee0bfb975fe7c04a55fcb3d6515
Laura <laurabeatriserafim@gmail.com>
2020-05-22 19:37:10 -0300

    feat: config cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 
@@ -12,6 +13,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
